9fbdcb8d3f41bda8d0d0e10f13fc5c07d9084ff1
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-03-28 19:53:46 -0400

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+  {
+    path: "/",
+    template: "<h1>Hola</h1>"
+  },
+  {
+    path: "/contacto",
+    template: "<h1>Contacto reynaldomoreno173@mail.com</h1>"
+  },
+  {
+    path: "/aboutme",
+    template: "<h1>About Me</h1>"
+  },
+];
+
